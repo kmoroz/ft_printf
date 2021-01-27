ecf1a0931e6388f6629799f2beb0d5aa46f43bc3
kmoroz <ksmorozova@hotmail.com>
2021-01-27 12:14:43 +0000

    remove 0 fill rule, not necessary for chars

--- processor/print_char.c
+++ processor/print_char.c
@@ -6,7 +6,7 @@
 /*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/26 18:09:50 by ksmorozo      #+#    #+#                 */
-/*   Updated: 2021/01/26 18:32:17 by ksmorozo      ########   odam.nl         */
+/*   Updated: 2021/01/27 12:12:56 by ksmorozo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,7 @@ void	print_char(va_list *arguments, t_recipe recipe)
 	else
 		c = (char)va_arg(*arguments, int);
 	if (recipe.width && !recipe.minus_flag)
-	{
-		if (recipe.zero_flag)
-			write_padding('0', recipe.width - 1);
-		else
-			write_padding(' ', recipe.width - 1);
-	}
+		write_padding(' ', recipe.width - 1);
 	write(1, &c, 1);
 	if (recipe.width && recipe.minus_flag)
 		write_padding(' ', recipe.width - 1);
