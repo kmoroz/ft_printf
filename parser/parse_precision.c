4fceaf5eb4bcaa0d6341319c2782a9134dd6e9b4
kmoroz <ksmorozova@hotmail.com>
2021-02-04 15:45:22 +0000

    fix precision parcing bug

--- parser/parse_precision.c
+++ parser/parse_precision.c
@@ -6,7 +6,7 @@
 /*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/26 17:26:04 by ksmorozo      #+#    #+#                 */
-/*   Updated: 2021/01/29 17:13:47 by ksmorozo      ########   odam.nl         */
+/*   Updated: 2021/02/04 15:43:33 by ksmorozo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,22 @@ int	parse_precision(va_list *arguments, t_recipe *recipe, const char **print_me)
 	{
 		(*print_me)++;
 		if (ft_isalpha(**print_me))
+			recipe->null_precision = 1;
+		if (**print_me == '0')
 		{
 			recipe->null_precision = 1;
-			return (1);
+			(*print_me)++;
 		}
-		if (**print_me == '0')
-			recipe->null_precision = 1;
 		if (**print_me == '*')
+		{
 			recipe->precision = va_arg(*arguments, int);
-		else if (ft_isdigit(**print_me) && **print_me != '0')
+			(*print_me)++;
+		}
+		while (ft_isdigit(**print_me) && !recipe->null_precision)
+		{
 			recipe->precision = (recipe->precision * 10) + (**print_me - '0');
-		(*print_me)++;
+			(*print_me)++;
+		}
 		return (1);
 	}
 	return (0);
