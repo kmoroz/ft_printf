a421640a24072352cac4237c4d682acea7c1215e
kmoroz <ksmorozova@hotmail.com>
2021-02-05 12:32:31 +0000

    remove linux specific printing

--- processor/print_ptr.c
+++ processor/print_ptr.c
@@ -6,7 +6,7 @@
 /*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/02/05 11:11:03 by ksmorozo      #+#    #+#                 */
-/*   Updated: 2021/02/05 12:23:16 by ksmorozo      ########   odam.nl         */
+/*   Updated: 2021/02/05 12:31:53 by ksmorozo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,6 @@ int	print_ptr(va_list *arguments, t_recipe recipe)
 	char				*address_str;
 
 	address_arg = va_arg(*arguments, unsigned long long);
-	if (!address_arg)
-	{
-		write(1, "(nil)", 5);
-		return (5);
-	}
 	address_str = ft_itoa_base(address_arg, 16, "0123456789abcdef");
 	length = ft_strlen(address_str) + 2;
 	if (!address_str)
