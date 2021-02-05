3d2aabc356cad53efda3e05b51847dec596ff872
kmoroz <ksmorozova@hotmail.com>
2021-02-05 10:13:41 +0000

    correct a mistake in percent

--- processor/print_percent.c
+++ processor/print_percent.c
@@ -6,7 +6,7 @@
 /*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/02/04 18:58:48 by ksmorozo      #+#    #+#                 */
-/*   Updated: 2021/02/04 19:09:13 by ksmorozo      ########   odam.nl         */
+/*   Updated: 2021/02/05 10:12:28 by ksmorozo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ int	print_percent(t_recipe recipe)
 	if (recipe.width && !recipe.minus_flag)
 	{
 		if (recipe.zero_flag)
-			write_padding('0', recipe.width--);
+			write_padding('0', recipe.width - 1);
 		else
-			write_padding(' ', recipe.width--);
+			write_padding(' ', recipe.width - 1);
 	}
 	write_padding('%', 1);
 	if (recipe.width && recipe.minus_flag)
-		write_padding(' ', recipe.width--);
+		write_padding(' ', recipe.width - 1);
 	if (recipe.width)
 		return (recipe.width);
 	return (1);
