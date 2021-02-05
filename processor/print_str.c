4d26f5a4d92d7f788e31b38e3f31544ac6b9bdfd
kmoroz <ksmorozova@hotmail.com>
2021-02-05 17:28:35 +0000

    remove comments

--- processor/print_str.c
+++ processor/print_str.c
@@ -6,7 +6,7 @@
 /*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/02/05 14:10:53 by ksmorozo      #+#    #+#                 */
-/*   Updated: 2021/02/05 17:25:30 by ksmorozo      ########   odam.nl         */
+/*   Updated: 2021/02/05 17:28:04 by ksmorozo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,7 @@
 static void	deal_with_left_pads(int *str_len, t_recipe recipe)
 {
 	if (recipe.precision > *str_len)
-	{
 		write_padding(' ', recipe.width - *str_len);
-		//write(1, str, ft_strlen(str));
-	}
 	if (recipe.precision && recipe.precision <= *str_len)
 	{
 		write_padding(' ', recipe.width - recipe.precision);
@@ -29,10 +26,7 @@ static void	deal_with_left_pads(int *str_len, t_recipe recipe)
 	if (recipe.null_precision)
 		write_padding(' ', recipe.width);
 	if (!recipe.precision && recipe.width && !recipe.null_precision)
-	{
 		write_padding(' ', recipe.width - *str_len);
-		//write(1, str, ft_strlen(str));
-	}
 }
 
 static void	deal_with_precision(int *str_len, t_recipe recipe)
