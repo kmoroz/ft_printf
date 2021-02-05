7aa4b9819fda2db3dbb2ad72d9d2c602da9fe81f
kmoroz <ksmorozova@hotmail.com>
2021-02-05 18:38:57 +0000

    cancel previous changes

--- parser/parse_precision.c
+++ parser/parse_precision.c
@@ -6,7 +6,7 @@
 /*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/26 17:26:04 by ksmorozo      #+#    #+#                 */
-/*   Updated: 2021/02/05 18:25:08 by ksmorozo      ########   odam.nl         */
+/*   Updated: 2021/02/05 18:38:14 by ksmorozo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,20 @@ int	parse_precision(va_list *arguments, t_recipe *recipe, const char **print_me)
 		(*print_me)++;
 		if (ft_isalpha(**print_me))
 			recipe->null_precision = 1;
+		while (**print_me == '0')
+		{
+			recipe->null_precision = 1;
+			(*print_me)++;
+		}
 		if (**print_me == '*')
 		{
 			recipe->precision = va_arg(*arguments, int);
-			if (recipe->precision == 0)
-				recipe->null_precision = 1;
 			(*print_me)++;
 		}
 		while (ft_isdigit(**print_me))
 		{
+			recipe->null_precision = 0;
 			recipe->precision = (recipe->precision * 10) + (**print_me - '0');
-			if (recipe->precision == 0)
-				recipe->null_precision = 1;
 			(*print_me)++;
 		}
 		return (1);
