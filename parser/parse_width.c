f74df111b5966e35f5bb4c8c4b80fd3c58c241fb
kmoroz <ksmorozova@hotmail.com>
2021-02-08 14:30:35 +0000

    edit width partcing to pass new unit tests in pft

--- parser/parse_width.c
+++ parser/parse_width.c
@@ -6,7 +6,7 @@
 /*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/26 17:27:04 by ksmorozo      #+#    #+#                 */
-/*   Updated: 2021/01/26 18:04:57 by ksmorozo      ########   odam.nl         */
+/*   Updated: 2021/02/08 14:26:19 by ksmorozo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,11 @@ int	parse_width(va_list *arguments, t_recipe *recipe, const char **print_me)
 	if (**print_me == '*')
 	{
 		width = va_arg(*arguments, int);
+		if (width < 0)
+		{
+			recipe->minus_flag = 1;
+			width = width * -1;
+		}
 		(*print_me)++;
 	}
 	while (ft_isdigit(**print_me))
