29d1a8c72e060f08e6509933ac70f49b52d3031c
kmoroz <ksmorozova@hotmail.com>
2021-02-12 17:08:01 +0000

    add length in char

--- processor/print_char.c
+++ processor/print_char.c
@@ -6,7 +6,7 @@
 /*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/26 18:09:50 by ksmorozo      #+#    #+#                 */
-/*   Updated: 2021/01/27 13:32:09 by ksmorozo      ########   odam.nl         */
+/*   Updated: 2021/02/12 16:27:40 by ksmorozo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	print_char(va_list *arguments, t_recipe recipe)
 	if (recipe.length == 'l')
 		c = va_arg(*arguments, wchar_t);
 	else
-		c = (char)va_arg(*arguments, int);
+		c = (wchar_t)va_arg(*arguments, int);
 	if (recipe.width && !recipe.minus_flag)
 		write_padding(' ', recipe.width - 1);
 	write(1, &c, 1);
