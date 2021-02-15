b243b0eaf68983ad76b5ef6083a4278a8e6305a9
kmoroz <ksmorozova@hotmail.com>
2021-02-15 13:33:28 +0000

    edit itoa protection

--- processor/print_ptr.c
+++ processor/print_ptr.c
@@ -6,7 +6,7 @@
 /*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/02/05 11:11:03 by ksmorozo      #+#    #+#                 */
-/*   Updated: 2021/02/05 12:31:53 by ksmorozo      ########   odam.nl         */
+/*   Updated: 2021/02/15 13:29:21 by ksmorozo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ int	print_ptr(va_list *arguments, t_recipe recipe)
 
 	address_arg = va_arg(*arguments, unsigned long long);
 	address_str = ft_itoa_base(address_arg, 16, "0123456789abcdef");
-	length = ft_strlen(address_str) + 2;
 	if (!address_str)
 		return (-1);
+	length = ft_strlen(address_str) + 2;
 	if (recipe.width && !recipe.minus_flag)
 		write_padding(' ', recipe.width - length);
 	write(1, "0x", 2);
